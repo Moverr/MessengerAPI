a96a9b6dcd059bebae51dd9d7ce9f9500c88765f
Moverr <moverr@gmail.com>
2022-04-18 22:03:07 +0300

    :Debuuggging:

--- App.py
+++ App.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def home():
-    return  render_template("welcome.html")
+    return  render_template('welcome.html')
