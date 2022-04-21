1193aa2a1ea67edc79cfdd6f6d640ae0a3f1cda8
Moverr <moverr@gmail.com>
2022-04-21 04:29:25 +0300

    :Debugging ,Basic setup for RAbbit  MQ :

--- services/MessengerService.py
+++ services/MessengerService.py
@@ -1,12 +1,25 @@
+
+#!/usr/bin/env python
+import pika
+
+
 class  MessengerService:
     
+
     def __init__(self, *args, **kwargs):
-         pass
+         self.rabbitMqConnection  =   pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+         self.channel = self.rabbitMqConnection.channel()
 
     
-    def sendMessage():
-        pass
+    def sendMessage(self):
+        self.channel.queue_declare(queue='hello')
+        self.channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+        print("[x] sent `Hello world!` ")
+        self.rabbitMqConnection.close()
 
     def receiveMessage():
         pass
     
+ 
