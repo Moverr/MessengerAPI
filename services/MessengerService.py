0c28c6d2e8bc9aa8276b04b89b3a485e00fd4fe0
Moverr <moverr@gmail.com>
2022-04-21 04:08:51 +0300

    :Debugging : init :

---
+++ services/MessengerService.py
@@ -0,0 +1,12 @@
+class  MessengerService:
+    
+    def __init__(self, *args, **kwargs):
+         pass
+
+    
+    def sendMessage():
+        pass
+
+    def receiveMessage():
+        pass
+    
---
+++ services/google/GoogleNotificationsService.py
@@ -0,0 +1,7 @@
+class GoogleNotificationSerice:
+    
+    def __init__(self, *args, **kwargs):
+         pass
+    
+    
+    
