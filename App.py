97ddaafb0340bb2f986cf0bc1d8aed986bcaca27
Moverr <moverr@gmail.com>
2022-04-24 00:40:41 +0300

    :Debugging : init :

--- App.py
+++ App.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/",methods=['GET'])
 def home():
     return  render_template('welcome.html')
+
+@app.route("/send",methods=['POST'])
+def send():
+    return   "Send"
+
+
+@app.route("/receive",methods=['POST'])
+def receive():
+    return  "Receive"
